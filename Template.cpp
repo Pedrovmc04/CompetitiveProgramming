0173f69d66b71c7e57c43e8075137142c5e0466b
Pedrovmc04 <pedrovmc132@gmail.com>
2021-09-26 17:20:00 -0300

    26/08/2021

---
+++ Template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define pb push_back
+
+typedef int64_t ll;
+
+const int maxn = 1e4;
+
+int32_t main()
+{
+    ios::sync_with_stdio(false),cin.tie(NULL);
+   
+    return 0;
+}	
